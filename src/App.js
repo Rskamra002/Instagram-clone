039971a658ae15649c309664275c68533aabdc78
unknown <1998ayushsinghal@gmail.com>
2021-05-24 21:26:08 +0530

    Login page slider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Routes from "./Routes/Routes";
-
+import Login from "./Components/Login/Login";
 function App() {
   return (
     <>
-      <Routes />
+      {/* <Routes /> */}
+      <Login/>
     </>
   );
 }
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Slider from './Slider/Slider'
 
 function Login() {
     return (
-        <div>
-            <div>Login</div>
+        <div style={{background:"rgb(250,250,250)"}}>
+
+            <div style={{width:"85%",margin:"auto",padding:"3rem"}}>
+                <Slider/>
+            </div>
         </div>
     )
 }
