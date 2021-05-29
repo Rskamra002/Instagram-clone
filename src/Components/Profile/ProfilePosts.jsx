4f22cff7205cd39c980629ddd49b700036bc74e4
Robin Singh <Rskamra002@gmail.com>
2021-05-29 22:09:56 +0530

    added profile posts & context provider

--- src/Components/Profile/ProfilePosts.jsx
+++ src/Components/Profile/ProfilePosts.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserPosts } from "../../Redux/UserProfile/action";
+import { Grid, Container } from "@material-ui/core";
+import DisplayPost from "./DisplayPost";
 
 function ProfilePosts() {
-    return (
-        <div>
-            <div>Profile Photos</div>
-        </div>
-    )
-}
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.profile.posts);
 
-export default ProfilePosts
+
+  useEffect(() => {
+    dispatch(getUserPosts(2));
+  }, []);
+
+
+  return (
+    <>
+      <Container>
+        <Grid container spacing={3}>
+
+          {posts?.map((post, id) => (
+            <DisplayPost key={id} {...post} />
+          ))}
+
+        </Grid>
+      </Container>
+    </>
+  );
+}
+export default ProfilePosts;
