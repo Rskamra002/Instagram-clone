ee10b10207801a00578e070b46886b65a27dff26
khushbookhator <khushbookhator11@gmail.com>
2021-05-29 17:11:38 +0530

    dropdowns

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import { Navbar } from "./Components/Navbar/Navbar";
 import Routes from "./Routes/Routes";
 
 function App() {
   return (
     <>
       <Routes />
-      {/* <Navbar/> */}
     </>
   );
 }
