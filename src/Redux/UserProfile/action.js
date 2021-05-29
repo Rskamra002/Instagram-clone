7090185caa17d179f3067ff9281302ca7214ecea
Robin Singh <Rskamra002@gmail.com>
2021-05-29 22:13:27 +0530

    Post Popup feature added

--- src/Redux/UserProfile/action.js
+++ src/Redux/UserProfile/action.js
@@ -2,6 +2,9 @@ import {
   USER_DATA_FAILURE,
   USER_DATA_REQUEST,
   USER_DATA_SUCCESS,
+  USER_POSTS_FAILURE,
+  USER_POSTS_REQUEST,
+  USER_POSTS_SUCCESS,
 } from "./actionType";
 import Axios from "axios";
 
@@ -29,19 +32,55 @@ const userDataFailure = (err) => {
   };
 };
 
+const userPostsRequest = () => {
+  return {
+    type: USER_POSTS_REQUEST,
+  };
+};
+
+const userPostsSuccess = (payload) => {
+  return {
+    type: USER_POSTS_SUCCESS,
+    payload,
+  };
+};
+
+const userPostsFailure = (err) => {
+  return {
+    type: USER_POSTS_FAILURE,
+    payload: err,
+  };
+};
+
+
 export const getUserData = (id) => (dispatch) => {
   dispatch(userDataRequest());
   const config = {
-    url: `/instaUsers?userId=${id}`,
+    url: `/instaUsers/${id}`,
     method: "get",
   };
   axios(config)
     .then((res) => {
-      console.log("dataressss", res.data);
       dispatch(userDataSuccess(res.data));
     })
     .catch((err) => {
       console.log(err);
       dispatch(userDataFailure(err));
+    });
+};
+
+export const getUserPosts = (id) => (dispatch) => {
+  dispatch(userPostsRequest());
+  const config = {
+    url: `/instaPosts?userId=${id}`,
+    method: "get",
+  };
+  axios(config)
+    .then((res) => {
+      dispatch(userPostsSuccess(res.data));
+    })
+    .catch((err) => {
+      console.log(err);
+      dispatch(userPostsFailure(err));
     });
 };
