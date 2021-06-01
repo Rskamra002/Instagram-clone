d53a724e62c2c12a5250fdd1cff9e9d49307cc77
Robin Singh <Rskamra002@gmail.com>
2021-06-01 14:51:12 +0530

    updated routes & redux of profile

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,31 +4,33 @@ import Home from '../Components/Home/Home'
 import { Navbar } from '../Components/Navbar/Navbar'
 import Registration from '../Components/Registration/Registration'
 import Login from "../Components/Login/Login"
-import {PrivateRoute} from "./PrivateRoute"
-import {Explore} from "../Components/Explore/Explore"
+import { PrivateRoute } from "./PrivateRoute"
+import { Explore } from "../Components/Explore/Explore"
+import Profile from '../Components/Profile/Profile'
 
 function Routes() {
     return (
         <>
-        <Switch>
-            <PrivateRoute path='/' exact >
-                <Navbar/>
-                <Home/>
-            </PrivateRoute>
-            <PrivateRoute path='/accounts/emailsignup'>
-                <Registration />
-            </PrivateRoute>
-            <Route path='/login'>
-                <Login />
-            </Route>
-            <PrivateRoute exact path="/explore">
-                <Navbar/>
-                <Explore/>
-            </PrivateRoute>
-            <Route exact path="/:username">
-                <Navbar/>
-            </Route>
-        </Switch>
+            <Switch>
+                <PrivateRoute path='/' exact >
+                    <Navbar />
+                    <Home />
+                </PrivateRoute>
+                <PrivateRoute path='/accounts/emailsignup'>
+                    <Registration />
+                </PrivateRoute>
+                <Route path='/login'>
+                    <Login />
+                </Route>
+                <PrivateRoute exact path="/explore">
+                    <Navbar />
+                    <Explore />
+                </PrivateRoute>
+                <PrivateRoute exact path="/:username">
+                    <Navbar />
+                    <Profile />
+                </PrivateRoute>
+            </Switch>
         </>
     )
 }
