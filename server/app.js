20bec863cf4f4f86bf58248f658eadb5c001f36a
unknown <1998ayushsinghal@gmail.com>
2021-06-13 20:39:20 +0530

    change the authentication with mongo atlas

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require("cors")
+
 // this is a function in which i have to write the path of .env file in key:value pair
 dotenv.config({ path: './.env' });
 const connect = require('./db/connect');
@@ -15,7 +17,7 @@ const app = express();
 
 // Middlewares
 app.use(express.json());
-
+app.use(cors())
 app.use(require('./router/auth'));
 app.use(require('./router/users'));
 app.use(require('./router/posts'));
