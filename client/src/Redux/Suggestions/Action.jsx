7dc5be1250d32e3374bc2ee4e19824e38961c515
khushbookhator <khushbookhator11@gmail.com>
2021-06-13 21:16:15 +0530

    api changes

--- client/src/Redux/Suggestions/Action.jsx
+++ client/src/Redux/Suggestions/Action.jsx
@@ -22,7 +22,7 @@ export const getUserFail = (error) => {
 
 export const getUsers = () => (dispatch) => {
     dispatch(getUserReq())
-    return axios.get("https://json-server-mocker-neeraj-data.herokuapp.com/instaUsers")
+    return axios.get("http://localhost:2511/users")
     .then(res => dispatch(getUserSuc(res.data)))
     .catch(err => dispatch(getUserFail(err)))
 }
