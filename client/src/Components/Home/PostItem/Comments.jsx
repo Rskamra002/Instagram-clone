0e169575aff7d86777a89487f7d76c129b52376f
Neeraj Rawat <neerajrawatnt@gmail.com>
2021-06-14 14:24:47 +0530

    add comment on posts feature added

--- client/src/Components/Home/PostItem/Comments.jsx
+++ client/src/Components/Home/PostItem/Comments.jsx
@@ -1,51 +1,30 @@
+import axios from 'axios'
 import React from 'react'
-import styled from 'styled-components'
+import { useState } from 'react'
+import { useEffect } from 'react'
 import {timeConverter} from '../../../Utils/timeConverter'
 
-export default function Comments({allComments,viewMore,setViewMore}) {
+export default function Comments({comment}) {
+
+  const [commentBy,setCommentBy] = useState("")
+
+  useEffect(() => {
+    axios.get(`http://localhost:2511/users/${comment.userId}`).then((res) => {
+      setCommentBy(res.data.data.username)
+    })
+  },[])
+
   return (
-    <>
-      {allComments.length > 2 ? <ViewMoreComments >
-            <button onClick={() => {setViewMore(!viewMore)}}>{viewMore ? "Hide comments": "View more comments"}</button>
-            </ViewMoreComments> : null}
-            <AllComments viewMore={viewMore}> 
-              {allComments?.map((item,index) => {
-                return(
-                  <div id={index}>
-                    <span>
-                      <span>{item.displayName}</span>
-                      {item.comment}
-                    </span>
-                    <span>
-                      {timeConverter(item.commentTime)}
-                    </span>
-                  </div>
-                )
-              })}
-            </AllComments>
-    </>
+         <>
+          <div id={comment._id}>
+            <span>
+              <span>{commentBy}</span>
+              {comment.comment}
+            </span>
+            <span>
+              {timeConverter(comment.commentTime)}
+            </span>
+          </div>
+      </>
   )
 }
-const ViewMoreComments = styled.div`
-  button{
-    cursor: pointer;
-    border:none;
-    background-color:transparent;
-  }
-`
-const AllComments = styled.div`
-  max-height:58px;
-  overflow-y: ${props => props.viewMore ? "scroll" :"hidden"} ;
-  div{
-    font-size:15px;
-    margin:6px 0px;
-    display:flex;
-    justify-content:space-between;
-    & > span > span{
-      font-weight:bold;
-      display:inline-block;
-      margin-right:4px;
-    }
-
-  }
-`
